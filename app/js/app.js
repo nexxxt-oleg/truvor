feef696f9809f98c51714b9df4e87e30b631a9ef
Олег <nexxxt.07@gmail.com>
2022-06-30 10:05:46 +0300

    30.06

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,7 @@ import '~/node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import '~/node_modules/@fancyapps/ui/dist/fancybox.umd.js'
 //import './smooth-scroll.polyfills.min.js'
 import './lazyload.min.js'
+//import './sticky-sidebar.min.js'
 //import './imask.min.js'
 
 
